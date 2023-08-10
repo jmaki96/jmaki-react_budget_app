cb4a64b3ddaf359553fa2a57560e2f7e762406e0
Jake Aki <j.m.aki.usa@gmail.com>
2023-08-10 16:02:12 -0400

    Adding prefix to allocation

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -58,7 +58,7 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                    <span>{currency}</span>
                     <input
                         required='required'
                         type='number'
