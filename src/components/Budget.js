67d533d4db917d638bdcf23302cbb6e275fb9b72
Jake Aki <j.m.aki.usa@gmail.com>
2023-08-10 15:06:03 -0400

    Adding budget features

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,32 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { dispatch, budget, expenses} = useContext(AppContext);
+
+    const setBudget = (newBudget) => {
+
+        if(newBudget < expenses) {
+            alert("The new budget must be greater then the current expenses  £"+expenses);
+            newBudget = budget;
+        }
+
+        const maximumBudget = 20000;
+        if (newBudget > maximumBudget) {
+            alert("The new budget must not exceed the maximum budget of £"+maximumBudget);
+            newBudget = budget;
+        }
+
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: {
+                budget: parseInt(newBudget)
+            }
+        });
+    }
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £</span><input type='number' onChange={setBudget(e.target.value)}>{budget}</input>
         </div>
     );
 };
